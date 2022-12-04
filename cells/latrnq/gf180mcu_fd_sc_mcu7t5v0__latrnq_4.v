02522b9abaf0002a39ae89c8a7b72ddec39aba2f
Marwan Abbas <marwaneltoukhy@aucegypt.edu>
2022-12-04 13:55:20 +0200

    added reg notifier

--- cells/latrnq/gf180mcu_fd_sc_mcu7t5v0__latrnq_4.v
+++ cells/latrnq/gf180mcu_fd_sc_mcu7t5v0__latrnq_4.v
@@ -25,6 +25,7 @@ module gf180mcu_fd_sc_mcu7t5v0__latrnq_4( E, D, RN, Q );
 `endif // If not USE_POWER_PINS
 input D, E, RN;
 output Q;
+reg notifier;
 
 `ifdef USE_POWER_PINS
   gf180mcu_fd_sc_mcu7t5v0__latrnq_func gf180mcu_fd_sc_mcu7t5v0__latrnq_inst(.E(E),.D(D),.RN(RN),.Q(Q),.VDD(VDD),.VSS(VSS),.notifier(notifier));
